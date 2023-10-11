925a806769aaf4c9fbe5dd489aa45b88840b62e2
primary-acct-usr <147643077+primary-acct-usr@users.noreply.github.com>
2023-10-11 13:34:43 -0500

    updaing html and css files

---
+++ select.js
@@ -0,0 +1,29 @@
+function objectHide() {
+    var x = document.getElementById("item2-exchange");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+function _objectHide() {
+    var y = document.getElementById("item3-exchange");
+    if (y.style.display === "none") {
+      y.style.display = "block";
+      x.style.display = "none";
+      a.style.display = "none";
+    } else {
+      y.style.display = "none";
+    }
+  }
+  function sobjectHide() {
+    var a = document.getElementById("tech");
+    if (a.style.display === "none") {
+      a.style.display = "block";
+    } else {
+      a.style.display = "none";
+    }
+  }
+
+  
+ 
